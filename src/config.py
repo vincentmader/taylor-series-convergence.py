5a3641185ada8b99b78afa1b27cb8cfb26aa7daf
Vincent C. Mader <vincent.mader@icloud.com>
2023-12-20 01:32:29 +0100

    FIX paths

--- src/config.py
+++ src/config.py
@@ -1,9 +1,9 @@
-
 import os
 import pathlib
+from pathlib import Path
 
 
 PATH_TO_PROJECT = pathlib.Path(__file__).parent.resolve()
 PATH_TO_PLOTS = os.path.join(PATH_TO_PROJECT, "../plots")
 
-MPL_THEME = "~/.config/matplotlib/dark.mplstyle"
+MPL_THEME = Path("..", "lib", "mpl-styles", "dark.mplstyle")
